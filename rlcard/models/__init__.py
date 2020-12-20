db0332ee60767aeddc57957f1affbba6d669dcca
samsvp <50253175+samsvp@users.noreply.github.com>
2020-12-20 18:55:00 -0300

    Update __init__.py

--- rlcard/models/__init__.py
+++ rlcard/models/__init__.py
@@ -4,13 +4,13 @@
 from rlcard.models.registration import register, load
 import subprocess
 import sys
+import tensorflow as tf
 from packaging import version
 
 reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
 if 'tensorflow' in installed_packages:
-    import tensorflow as tf
     if version.parse(tf.__version__) < version.parse('1.14.0') \
             or version.parse(tf.__version__) >= version.parse('2.0.0'):
         print('WAINING - RLCard supports Tensorflow >=1.14 and <2.0\nThe detected version is {} \nIf the models can not be loaded, please install Tensorflow via\n$ pip install rlcard[tensorflow]\n'.format(tf.__version__))
@@ -23,6 +23,10 @@ if 'torch' in installed_packages:
         model_id = 'leduc-holdem-nfsp-pytorch',
         entry_point='rlcard.models.pretrained_models:LeducHoldemNFSPPytorchModel')
 
+register(
+    model_id = 'limit-holdem-dqn',
+    entry_point='rlcard.models.pretrained_models:LimitHoldemDQNModel')
+    
 register(
     model_id = 'leduc-holdem-cfr',
     entry_point='rlcard.models.pretrained_models:LeducHoldemCFRModel')
