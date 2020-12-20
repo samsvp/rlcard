75de34172ef0f256645f1bc34c754a9d43249dcf
samsvp <50253175+samsvp@users.noreply.github.com>
2020-12-20 18:57:11 -0300

    Create LimitHoldemDQNModel Class

--- rlcard/models/pretrained_models.py
+++ rlcard/models/pretrained_models.py
@@ -4,12 +4,13 @@
 import os
 
 import rlcard
-from rlcard.agents import CFRAgent
+from rlcard.agents import CFRAgent, DQNAgent
 from rlcard.models.model import Model
+import tensorflow as tf
 
 # Root path of pretrianed models
 ROOT_PATH = os.path.join(rlcard.__path__[0], 'models/pretrained')
-
+print(os.path.join(ROOT_PATH, 'leduc_holdem_nfsp'))
 class LeducHoldemNFSPModel(Model):
     ''' A pretrained model on Leduc Holdem with NFSP
     '''
@@ -110,3 +111,44 @@ class LeducHoldemCFRModel(Model):
         '''
         return [self.agent, self.agent]
 
+
+class LimitHoldemDQNModel(Model):
+    ''' Trained Limit Holdem model with CFR
+    '''
+
+    ''' A pretrained model on Leduc Holdem with NFSP
+    '''
+
+    def __init__(self):
+        ''' Load pretrained model
+        '''
+        self.graph = tf.Graph()
+        self.sess = tf.Session(graph=self.graph)
+        env = rlcard.make('limit-holdem')
+        with self.graph.as_default():
+            self.dqn_agents = []
+            agent = DQNAgent(self.sess,
+                              scope='dqn',
+                              action_num=env.action_num,
+                              replay_memory_init_size=1000,
+                              train_every=1,
+                              state_shape=env.state_shape,
+                              mlp_layers=[512,512])
+            self.dqn_agents.append(agent)
+
+        check_point_path = os.path.join(ROOT_PATH, 'limit_holdem_dqn')
+        with self.sess.as_default():
+            with self.graph.as_default():
+                print(2)
+                saver = tf.train.Saver()
+                saver.restore(self.sess,tf.train.latest_checkpoint(check_point_path))
+    
+    @property
+    def agents(self):
+        ''' Get a list of agents for each position in a the game
+        Returns:
+            agents (list): A list of agents
+        Note: Each agent should be just like RL agent with step and eval_step
+              functioning well.
+        '''
+        return self.dqn_agents
